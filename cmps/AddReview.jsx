7f7e57fe8dcc2cafae173357ad58652a940c3114
Your Name <dan.weibren@gmail.com>
2025-06-22 20:36:21 +0300

    refactor(add-review): convert review form to controlled component
    - Replaced individual rate state with full review object state
    - Introduced dynamic handleChange function for inputs
    - Updated inputs to controlled components using value and onChange

--- cmps/AddReview.jsx
+++ cmps/AddReview.jsx
@@ -1,37 +1,35 @@
 
 import { appService } from "../services/books.service.js"
 import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
+import { utilService } from "../services/util.service.js"
 
-
-const { useNavigate, useParams } = ReactRouterDOM
 const { useRef, useState, useEffect } = React
+const { useParams, useOutletContext } = ReactRouterDOM
+
 
 export function AddReview() {
-    const curDate = new Date().toLocaleDateString('en-GB')
-    const emptyReview = { fullName: '', rate: '', date: new Date().toLocaleDateString('en-GB') }
 
+    const curDate = new Date().toLocaleDateString('en-GB')
+    const emptyReview = { reviewId: utilService.makeId(), fullName: '', rate: '', date: new Date().toLocaleDateString('en-GB') }
+
+    const { setBook } = useOutletContext()
     const { bookId } = useParams()
     const rateRef = useRef()
     const fullNameRef = useRef()
-    const navigate = useNavigate()
 
-    const [bookToReview, setBookToReview] = useState()
     const [review, setReview] = useState(emptyReview)
+    const [bookToEdit, setBookToEdit] = useState()
 
     useEffect(() => {
-        if (!bookToReview) {
-            appService.get(bookId)
-                .then(setBookToReview)
-                .catch(err => {
-                    showErrorMsg('Problem geting books')
-                    console.log('err', err)
-                })
-        }
-        else setBookToReview(prevBookToEdit => ({ ...prevBookToEdit, reviews: review }))
+        appService.get(bookId)
+            .then(setBookToEdit)
+    }, [])
+
+    useEffect(() => {
+        setBookToEdit(bookToEdit => ({ ...bookToEdit, reviews: [{ ...review, review }] }))
     }, [review])
 
     function handleChange({ target }) {
-
         const field = target.name
         let value = target.value
         switch (target.type) {
@@ -44,44 +42,34 @@ export function AddReview() {
                 value = target.checked
                 break
         }
-
         setReview(prevReview => ({ ...prevReview, [field]: value }))
-    }
-
-
-    function getStars() {
-        return '⭐'.repeat(review.rate);
     }
 
     function onSaveReview(ev) {
         ev.preventDefault()
-        if (!review.rate) return showErrorMsg('Review not saved! No rating interd')
-        appService.save(bookToReview)
-            .then(() => {
-                showSuccessMsg(`Book review saved!`)
-                navigate('/books')
-            })
-            .catch(err => {
-                showErrorMsg('Problem saving book review')
-                console.log(err)
-            })
+        if (!review.rate) return showErrorMsg('Review not saved! No rating entered')
+        appService.addReview(bookId, review).then(savedBooks => setBook(savedBooks))
+        setReview(emptyReview)
     }
 
     function onRemoveReview(ev) {
         ev.preventDefault()
+        showSuccessMsg('Book review discarded')
         setReview(emptyReview)
     }
+
     const reviewValue = (!review.fullName) ? '' : review.fullName
     const reviewRate = (!review.rate) ? 1 : review.rate
+    
     return (
         <form onSubmit={onSaveReview} className="add-review container">
             <h1> Add a review </h1>
             <time dateTime="rate-time"> Current date : {curDate}</time>
             <section className="inputs">
                 <label htmlFor="fullName"></label>
-                <input onChange={handleChange} ref={fullNameRef} className="full-name" type="text" id="fullName" name="fullName" value={reviewValue} placeholder=" What's your name?" />
-                <label htmlFor="rate">{getStars()}</label>
-                <input onChange={handleChange} ref={rateRef} type="range" id="rate" name="rate" min={1} max={5} value={reviewRate} placeholder=" So how do you rate this book from 1-5? " />
+                <input onChange={handleChange} ref={fullNameRef} className="full-name" type="text" id="fullName" name="fullName" value={reviewValue} placeholder="What's your name?" />
+                <label htmlFor="rate">{utilService.getStars(review.rate)}</label>
+                <input onChange={handleChange} ref={rateRef} type="range" id="rate" name="rate" min={1} max={5} value={reviewRate} placeholder="So how do you rate this book from 1-5? " />
             </section>
             <section className="actions">
                 <button className="add-review">Save</button>
